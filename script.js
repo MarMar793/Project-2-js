2173171407cad3bce1350a84ae5f049b40f8d952
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2023-09-26 20:33:14 +0100

    Update script.js
    Changed aspects of js to the following :
    - the code registers what number the user has guessed.
    -if user has guessed correct , they get a "winning" message"
    -if they guess incorrect they get a "losing " message.
    
    Will develop score more

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-/* The below registeres a score each time the user clicks */
-const clickButton = document.getElementById("clickButton");
-        const scoreElement = document.getElementById("score");
+/*getting the code to randomly generate a number between 1 and 10*/
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+/*changing the the const to register the users guess and for code to get answer*/
+submitGuess.addEventListener("click", () => {
+    // Get the user's guess from the input field
+    const userGuess = parseInt(guessInput.value);
+
+        /* Adding an else if to feedback to the user that they got the rght anser (or not)*/
+function handleGuess(userGuess) {
+    if (userGuess === randomNumber) {
+        }
+            messageElement.textContent = `Winner! The correct number was ${randomNumber}. It took you ${attempts} attempts.`;
+        }else {
+        messageElement.textContent = `Wrong answer. Try again!`;
 
         /* Start with a score of 0*/
         let score = 0;
@@ -8,7 +22,5 @@ const clickButton = document.getElementById("clickButton");
         /*Code to register when the user has clicked button & increase with each click*/
         clickButton.addEventListener("click", () => {
             score++;
-            
-            /*show score on page*/
-            scoreElement.textContent = score;
+         
         });
