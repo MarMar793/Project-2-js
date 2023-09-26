4d658595213173e8b5faaed2c583af7f083f741e
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2023-09-26 20:44:09 +0100

    Update script.js
    Added:
    Number buttons, 
    A hint in the return message when user guesses incorrectly (too low or too high)
    Restart button and functions

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
-/*getting the code to randomly generate a number between 1 and 10*/
+/*Generate a random number between 1 and 10*/
+let randomNumber = generateRandomNumber();
+
+/*Connects to HTML */
+const numberContainer = document.getElementById("numberContainer");
+const messageElement = document.getElementById("message");
+const resetButton = document.getElementById("resetButton");
+
+/*start with 0 attempts made*/
+let attempts = 0;
+
+/*Code generates a random number between 1 and 10*/
 function generateRandomNumber() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-/*changing the the const to register the users guess and for code to get answer*/
-submitGuess.addEventListener("click", () => {
-    // Get the user's guess from the input field
-    const userGuess = parseInt(guessInput.value);
-
-        /* Adding an else if to feedback to the user that they got the rght anser (or not)*/
+/*added else if to display message depening on users guess*/
 function handleGuess(userGuess) {
     if (userGuess === randomNumber) {
-        }
-            messageElement.textContent = `Winner! The correct number was ${randomNumber}. It took you ${attempts} attempts.`;
-        }else {
-        messageElement.textContent = `Wrong answer. Try again!`;
+        messageElement.textContent = `Winner winner chicken dinner! The right answer was ${randomNumber}. You guessed it in ${attempts} attempts.`;
+        disableNumbers(); // Disable number buttons after a correct guess
+        resetButton.disabled = false; // Enable the reset button
+    } else if (userGuess < randomNumber) {
+        messageElement.textContent = "Too low. Try again!";
+    } else if (userGuess > randomNumber) {
+        messageElement.textContent = "Too high. Try again!";
+    }
 
-        /* Start with a score of 0*/
-        let score = 0;
+    /*adds number of attempts*/
+    attempts++;
+}
 
-        /*Code to register when the user has clicked button & increase with each click*/
-        clickButton.addEventListener("click", () => {
-            score++;
-         
+/*create numbers with html*/
+function createNumberButtons() {
+    for (let i = 1; i <= 10; i++) {
+        const numberButton = document.createElement("button");
+        numberButton.textContent = i;
+
+        /*Registers users guess*/
+        numberButton.addEventListener("click", () => {
+            const userGuess = parseInt(numberButton.textContent);
+            handleGuess(userGuess);
         });
+        numberContainer.appendChild(numberButton);
+    }
+}
+
+/*disable buttons when user guesses correct*/
+function disableNumbers() {
+    const numberButtons = document.querySelectorAll("#numberContainer button");
+    numberButtons.forEach((button) => {
+        button.disabled = true;
+    });
+}
+
+/*This resets the game*/
+function resetGame() {
+    randomNumber = generateRandomNumber(); // Generate a new random number
+    messageElement.textContent = ""; // Clear the message
+    attempts = 0; // Reset the number of attempts
+    enableNumbers(); // Enable number buttons
+}
+
+/*Add reset button*/
+resetButton.addEventListener("click", () => {
+    resetGame();
+});
+
+/*Recreate numbers. Start game again*/
+createNumberButtons();
+resetGame(); // Initialize the game
