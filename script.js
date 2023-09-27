197e69f847bc8194d28c900cded99d5db97a9256
MarMar793 <137117772+MarMar793@users.noreply.github.com>
2023-09-27 01:56:36 +0100

    Update script.js
    fixing deployment issues

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
-/*number of attempts*/
-let remainingAttempts = 4; 
-/*Log randomly chosen number (winning number)*/
-let randomNumber; 
+let remainingAttempts = 4; // Change the number of attempts as needed
+let randomNumber; // To store the random number
 
-/*get from HTML*/
 const attemptsElement = document.getElementById("attemptsRemaining");
 const numberContainer = document.getElementById("numberContainer");
 const messageElement = document.getElementById("message");
 const resetButton = document.getElementById("resetButton");
 
-/*start with 0 attempts made by user*/
 let attempts = 0;
 
-/* generate a number randomly between 1-12*/
 function generateRandomNumber() {
     return Math.floor(Math.random() * 12) + 1; // Generates a random number between 1 and 12
 }
-/*no. of attempts remaining to be displayed*/
+
 function updateAttemptsDisplay() {
     attemptsElement.textContent = `You have ${remainingAttempts} attempts remaining`;
 }
 
-/* What should be displayed depending on if the user chose correctly or not*/
 function handleGuess(userGuess) {
     if (userGuess === randomNumber) {
-        
-        /*check if the user had 1 guess left when they chose the correct random numnber*/
+        // Check if this was the last attempt before displaying the win message
         if (remainingAttempts === 1) {
             messageElement.textContent = `Winner! The correct number was ${randomNumber}. You got it on the last attempt!`;
         } else {
             messageElement.textContent = `Winner! The correct number was ${randomNumber}. It took you ${attempts} attempts.`;
         }
-        disableNumbers(); /* disable numbers if they guessed correctly*/
+        disableNumbers();
         resetButton.disabled = false;
     } else if (userGuess < randomNumber) {
         messageElement.textContent = "Too low! Try again.";
@@ -39,36 +32,23 @@ function handleGuess(userGuess) {
         messageElement.textContent = "Too high! Try again.";
     }
 
-    /* log number of guesses made*/
     attempts++;
-    /*log number of guesses left*/
     remainingAttempts--;
 
-    updateAttemptsDisplay(); /* Update and display remaining attempts*/
+    updateAttemptsDisplay(); // Update and display remaining attempts
 
     if (remainingAttempts === 0 && userGuess !== randomNumber) {
         disableNumbers();
-        messageElement.textContent = `Out of attempts. Game over. The number was ${randomNumber}.`; /* tell user they lose if they don't guess the correct number */
+        messageElement.textContent = `Out of attempts. Game over. The number was ${randomNumber}.`;
         resetButton.disabled = false;
     }
 }
 
-/*conditions in which to disable numbers*/
-function disableNumbers() {
-    const numberButtons = document.querySelectorAll(".number-button");
-    numberButtons.forEach((button) => {
-        button.disabled = true;
-    });
-
-    
-/*create number buttons*/
 function createNumberButtons() {
-    for (let i = 1; i <= 12; i++) { // number range is 1-12
+    for (let i = 1; i <= 12; i++) { // Change the loop limit to 12
         const numberButton = document.createElement("button");
         numberButton.textContent = i;
         numberButton.classList.add("number-button");
-
-        /*each "click" on a number is a guess. log guess*/
         numberButton.addEventListener("click", () => {
             const userGuess = parseInt(numberButton.textContent);
             handleGuess(userGuess);
@@ -77,20 +57,22 @@ function createNumberButtons() {
     }
 }
 
-
-/*reset game*/
 function resetGame() {
     randomNumber = generateRandomNumber();
     messageElement.textContent = "";
     attempts = 0;
     enableNumbers();
     resetButton.disabled = true;
-    remainingAttempts = 4; // Reset attempts
+    remainingAttempts = 4; // Reset the attempts
     updateAttemptsDisplay(); // Update and display remaining attempts
 }
 
-
-
+function disableNumbers() {
+    const numberButtons = document.querySelectorAll(".number-button");
+    numberButtons.forEach((button) => {
+        button.disabled = true;
+    });
+}
 function enableNumbers() {
     const numberButtons = document.querySelectorAll(".number-button");
     numberButtons.forEach((button) => {
